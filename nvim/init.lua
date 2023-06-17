35cd4543f5056ab5c92be4c8290cb8de5cd36dc2
guodongq <goldon.qian@gmail.com>
2023-06-17 11:46:01 +0800

    refactor nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,12 @@
-require('config')
+vim.g.mapleader = [[ ]]
+vim.g.maplocalleader = [[ ]]
+
+local modules = {
+    'config.options',
+    'config.plugins',
+    'config.autocmds'
+}
+
+for _, module in ipairs(modules) do
+    require(module)
+end
