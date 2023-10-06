fcc5afa0fd1e7858928ca5de02a6d453ce12cffc
guodongq <goldon.qian@gmail.com>
2023-10-06 23:29:29 +0800

    update toggleterm using horizontal

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -12,7 +12,7 @@ function M.config()
     start_in_insert = true,
     insert_mappings = true,
     persist_size = true,
-    direction = "float",
+    direction = "horizontal", -- float, horizontal
     close_on_exit = true,
     shell = vim.o.shell,
     float_opts = {
