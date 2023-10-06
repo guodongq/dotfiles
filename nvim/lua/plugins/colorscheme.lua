6f9c2ec9141b65f2a9356f6a360a61e2b684034f
guodongq <goldon.qian@gmail.com>
2023-10-07 01:01:53 +0800

    fix symbols-outline icons error

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,24 +1,25 @@
 -- tokyonight
---local M = {
---    "folke/tokyonight.nvim",
---    lazy = false,
---    priority = 1000,
---    enabled = false,
---}
---M.name = "tokyonight"
---function M.config()
---    vim.cmd.colorscheme(M.name)
---end
+local M = {
+   "folke/tokyonight.nvim",
+   lazy = false,
+   priority = 1000,
+   enabled = true,
+}
+M.name = "tokyonight"
+function M.config()
+   -- vim.cmd.colorscheme(M.name)
+   vim.cmd.colorscheme("tokyonight-night")
+end
 
 -- sakura
-local M = {
-  "numToStr/Sakura.nvim",
-  lazy = false,
-  priority = 1000,
-}
-function M.config()
-  require("Sakura").load()
-end
+-- local M = {
+--   "numToStr/Sakura.nvim",
+--   lazy = false,
+--   priority = 1000,
+-- }
+-- function M.config()
+--   require("Sakura").load()
+-- end
 
 
 return M
