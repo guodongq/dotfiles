56d9fe188709ac3f7736f5ce7ab73ef9f5820f6b
guodongq <goldon.qian@gmail.com>
2023-10-31 17:24:24 +0800

    update nvim version

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,24 +1,29 @@
+-- ==================
 -- tokyonight
--- local M = {
---    "folke/tokyonight.nvim",
---    lazy = false,
---    priority = 1000,
---    enabled = true,
--- }
--- M.name = "tokyonight"
---
--- function M.config()
---   vim.cmd.colorscheme(M.name)
--- end
-
--- sakura
+-- ==================
 local M = {
-	"numToStr/Sakura.nvim",
+	"folke/tokyonight.nvim",
 	lazy = false,
 	priority = 1000,
+	enabled = true,
+	opts = { style = "moon" },
 }
+M.name = "tokyonight"
+
 function M.config()
-	require("Sakura").load()
+	vim.cmd.colorscheme(M.name)
 end
+
+-- ==================
+-- sakura
+-- ==================
+-- local M = {
+-- 	"numToStr/Sakura.nvim",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- }
+-- function M.config()
+-- 	require("Sakura").load()
+-- end
 
 return M
