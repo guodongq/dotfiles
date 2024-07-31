5580e43b5e1933860e991ebd9de17d3d566150b2
guodongq <goldon.qian@gmail.com>
2024-07-31 18:00:10 +0800

    update dotfiles

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -21,7 +21,7 @@ function M.opts()
 	dashboard.section.buttons.val = {
 		dashboard.button("f", " " .. " Search Files", ":Telescope find_files<CR>"),
 		dashboard.button("g", " " .. " Search by Grep", ":Telescope live_grep <CR>"),
-		dashboard.button("p", " " .. " Search Projects", ":Telescope projects<CR>"),
+		dashboard.button("p", " " .. " Search Projects", ":Telescope project<CR>"),
 		dashboard.button("r", " " .. " Search Recent Files", ":Telescope oldfiles <CR>"),
 		dashboard.button("n", " " .. " New File", [[<cmd> ene <BAR> startinsert <cr>]]),
 		dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
